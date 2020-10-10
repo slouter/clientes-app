45ca7c450375504a602e8a2bb77f4b96e2238c5d
bryan mauriz <bryanmauriz@gmail.com>
2020-10-10 13:50:16 -0500

    2commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a angular';
+  curso : string = 'Angular 7 + Spring 5';
+  alumno : string = 'Bryan Mauriz Arizaga';
 }
